6a0da9f4d2edea8a5b93e601f74556f24595093f
sabbir-shuvo-ux <shuvosabbir134b@gmail.com>
2023-01-19 15:54:38 +0600

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors")
 
 const app = express();
 mongoose.set('strictQuery', false);
@@ -14,6 +15,7 @@ const PORT = process.env.PORT;
 
 
 //MIDDLEWARE
+app.use(cors())
 app.use(express.json());
 app.use((req, res, next) => {
       console.log(req.path, "||", req.method);
